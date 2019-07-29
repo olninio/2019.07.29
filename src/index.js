e31af202ab1f3b3edc4eccae17024597aefc5a29
Amazing Design <github@amazingdesign.eu>
2019-07-29 10:51:05 +0200

    Calling/invoke component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = () => <h1>Hello</h1>
+const Hello = () =>  <h1>Hello</h1>
+const hello = <h1>Hello</h1>
+
+console.log(Hello)
+console.log(hello)
+console.log(Hello())
+console.log(<Hello />)
 
 ReactDOM.render(
   <Hello />,

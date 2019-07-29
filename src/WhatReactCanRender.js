307601dacb90de5e5644d209295d741870da4ef3
Amazing Design <github@amazingdesign.eu>
2019-07-29 11:18:48 +0200

    Data types in JS

--- src/WhatReactCanRender.js
+++ src/WhatReactCanRender.js
@@ -1,8 +1,28 @@
 import React from 'react'
 
+const number = 1
+const string = 'cokolwiek'
+const bool = true
+const nullVar = null
+const undef = undefined
+
+const obj1 = {}
+const obj2 = { name: 'Mateusz' }
+
+const arr1 = []
+const numbers = [1, 2, 3]
+const names = ['Ala', 'Ola', 'Ela']
+const objects = [
+  { name: 'Ala' },
+  { name: 'Ela' },
+  { name: 'Ola' }
+]
+
+const hello = () => 'Hello'
+
 const WhatReactCanRender = () => (
   <div>
-    Mateusz
+
   </div>
 )
 

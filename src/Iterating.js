a467c64fd6e1984c8703c23e31fe5f50ae33842f
Amazing Design <github@amazingdesign.eu>
2019-07-29 12:13:04 +0200

    Using map and filter to iterating an array

--- src/Iterating.js
+++ src/Iterating.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
+console.log([
+  <p>Ala</p>,
+  <p>Ola</p>,
+  <p>Ela</p>,
+])
+
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 const evenNumbers = numbers.filter(number => number % 2 === 0)
+
+const names = ['Ala', 'Ola', 'Ela']
 
 const Iterating = (props) => (
   <div>
@@ -14,6 +22,19 @@ const Iterating = (props) => (
         .filter(number => number % 2 !== 0)
         .map(number => 2 * number + ' ')
     }
+    <br />
+    {
+      names.map(name => React.createElement('p', {}, name))
+    }
+    {
+      names.map(name => <p>{name}</p>)
+    }
+    <br />
+    <ul>
+      {
+        names.map(name => <li>{name}</li>)
+      }
+    </ul>
   </div>
 )
 

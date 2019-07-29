95e0995b5e058fc770ee17abc8cbef7188da9705
Amazing Design <github@amazingdesign.eu>
2019-07-29 11:55:41 +0200

    Basic iterating

--- src/Iterating.js
+++ src/Iterating.js
@@ -1,8 +1,19 @@
 import React from 'react'
 
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+const evenNumbers = numbers.filter(number => number % 2 === 0)
+
 const Iterating = (props) => (
   <div>
-    Iterating
+    {numbers}
+    <br />
+    {evenNumbers}
+    <br />
+    {
+      numbers
+        .filter(number => number % 2 !== 0)
+        .map(number => 2 * number + ' ')
+    }
   </div>
 )
 
